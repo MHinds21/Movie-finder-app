f3575ab20fd8c8c3c77462cf8971eb7a69d66b20
MHinds21 <m26577169@gmail.com>
2026-04-03 16:37:45 -0400

    Fix console log formatting in MovieSearch function

--- frontend/frontend/app.js
+++ frontend/frontend/app.js
@@ -1,4 +1,4 @@
 functionMovieSearch(){
 cont query =   let container = document.getElementById("SearchInput").value;
-    console.log( "Searching for: "query);
+    console.log( "Searching for:" query);
 }
